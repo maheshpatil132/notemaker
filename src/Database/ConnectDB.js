343b1e8aba435a73a97c4ca504c6dd1865104a11
mahesh patil <92781925+maheshpatil132@users.noreply.github.com>
2023-12-18 14:33:26 +0530

    updates in .env

--- src/Database/ConnectDB.js
+++ src/Database/ConnectDB.js
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js'
 
 
 const supabaseUrl = process.env.Supabase_URL
-const supabaseKey = process.env.supabase_KEY
+const supabaseKey = process.env.Supabase_KEY
 
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
