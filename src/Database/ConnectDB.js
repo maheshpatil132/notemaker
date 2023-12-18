1964dc710131eaf001f79a823279bff2dee9fa0a
mahesh patil <92781925+maheshpatil132@users.noreply.github.com>
2023-12-18 14:28:01 +0530

    set up Enviroment variables

--- src/Database/ConnectDB.js
+++ src/Database/ConnectDB.js
@@ -2,8 +2,8 @@
 import { createClient } from '@supabase/supabase-js'
 
 
-const supabaseUrl = 'https://teqpqjmsietmaaxkbbzo.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRlcXBxam1zaWV0bWFheGtiYnpvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDI1NjEwMTQsImV4cCI6MjAxODEzNzAxNH0.kNmf0fDTgnOw_F-RS8IcGL81foqQ7VGGw5E8CGxf-KE'
+const supabaseUrl = process.env.Supabase_URL
+const supabaseKey = process.env.supabase_KEY
 
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
